83f5da335032461c940f16bdf3d0322ae4789926
Arunaabh Pant <ap1933@scarletmail.rutgers.edu>
2021-12-05 22:00:49 -0500

    add data collection module

--- Main/data_collection.py
+++ Main/data_collection.py
@@ -6,7 +6,7 @@ from Entity.Grid import Grid
 from Main.Search import *
 from Main.voyage import get_default_states
 
-PROBABILITIES = [0.06, 0.12, 0.18, 0.24, 0.3, 0.36, 0.42, 0.45, 0.48]
+PROBABILITIES = [0.06, 0.12, 0.18, 0.24, 0.3, 0.36, 0.42, 0.48]
 TEST_COUNT = GLOBAL_TEST_COUNT
 GRID_SIZE = GLOBAL_BIG_MAZE_SIZE
 # for q6 we allow field of view and for q7 we restrict it

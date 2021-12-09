4932d8c9147a7ee36bb3409ed5e64700bb238914
Arunaabh Pant <ap1933@scarletmail.rutgers.edu>
2021-12-09 01:52:20 -0500

    move to hdf file format

--- Main/data_collection.py
+++ Main/data_collection.py
@@ -1,14 +1,12 @@
 # call solve_maze and pass field_view = 1
-import csv
 import pandas as pd
 import numpy as np
-import json
 
 from Entity.Grid import Grid
 from Main.Search import *
 from Main.voyage import get_default_states
 
-PROBABILITIES = [0.06, 0.12, 0.18, 0.24, 0.3, 0.36, 0.42, 0.48]
+PROBABILITIES = [0.3]
 TEST_COUNT = GLOBAL_TEST_COUNT
 GRID_SIZE = GLOBAL_BIG_MAZE_SIZE
 # for q6 we allow field of view and for q7 we restrict it
@@ -44,15 +42,11 @@ def write_to_file(data):
         g = Grid.make_grid(0, GLOBAL_BIG_MAZE_SIZE)
         idxs = data[i][0]
         g[idxs[0]][idxs[1]] = 1
-        data[i][0] = json.dumps(g.astype(int).tolist())
-        data[i][1] = json.dumps(data[i][1].astype(int).tolist())
+        data[i][0] = g
         data[i][2] = map_direction(data[i][2])
         i += 1
-    file = open(r'../Data/data-train-1.csv', 'a')
-    with file:
-        write = csv.writer(file, delimiter=',')
-        write.writerows(data)
-    file.close()
+    data = pd.DataFrame(data)
+    data.to_hdf('../Data/train-1.h5', key='df', mode='w')
 
 
 def map_direction(inp):
